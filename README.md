fc55b4deee2346459877ddf0b3924cafb02a73ba
Vhyme <vhyme@live.cn>
2017-05-25 12:49:03 +0800

    x

--- README.md
+++ README.md
@@ -1,67 +1,8 @@
 SeuJwcFker
 ============
-###更新记录
-0. 2014-05-30 创世，实现同院竞争臭表脸模式
-1. 2014-05-31 代码重写，优化交互，实现人文类课程的孤注一掷模式
-2. 2014-06-01 实现人文类课程的暴力模式
-3. 2014-09-02 将py脚本封装成为.exe格式，方便无python环境的用户使用
-1. 2014-12-26 修改学期选择处的一个提示错误： 短学期输入1，秋季学期为2，**春季学期应输入3**
 
-###SeuJwcFker如何使用？
-方法一：使用封装好的.exe文件(**不需python环境**)
+## 2017/5/25 变态版
 
-1. [点击这里](https://github.com/SnoozeZ/seu-jwc-fker/raw/master/seu_jwc_fker.rar)下载工具压缩包
-2. 解压下载下来的压缩包，运行其中的"seu_jwc_fker.exe"，然后根据提示操作
-
-方法二：直接使用python脚本
-
-1. 安装Python环境(推荐Python 2.7)
-2. [点击这里](https://raw.githubusercontent.com/SnoozeZ/seu-jwc-fker/master/seu_jwc_fker.py)下载脚本，python解释器中运行之，然后根据提示操作
-
-
-###SeuJwcFker是什么，能实现什么功能？
-SeuJwcFker是一个辅助同学们在东南大学选课系统中顺利选课的工具，可以自动值守空闲课程并注册。目前可以实现以下功能：
-* 同院竞争臭表脸模式：值守主界面本院的所有“服从推荐”类课程
-* 人文孤注一掷模式：只值守子界面“人文社科类”中你指定一门课程
-* 人文暴力模式：值守子界面“人文社科类”所有课程，有剩余就选上
-
-###SeuJwcFker还需要改进的地方：
-* 支持任意用户指定的一门~~人文社科类~~、自然类课程的值守
-* 支持任意门用户指定课程的值守
-
-
-
-
-
-
-###其他
-欢迎pull-request，虽然我知道当年这个代码写得很烂...
-使用中若遇到问题请提交issue或联系Email: zhangweiqi357@gmail.com
-
-本工具遵循 [WTFPL V2](http://www.wtfpl.net/txt/copying/) 协议
-
-***
-
-###What is the SeuJwcFker?
-SeuJwcFker is a tool that helps  students in SEU to efficiently regsiter their courses.
-
-
-###What can it do?
-It can help you to login into the course registering system, and register the courses if there are left courses.
-
-###Need to be done:
-* Adopt the tech of Captcha Recognition to support more kind of courses.
-* Some bugs may exist.
-
-###How to use? 
-1. Install Python development environment. Python 2.7 is recommend.
-2. Execute *python seu_jwc_fker.py* in your terminal and then follow the instruction.
-
-###License
-Licensed under [WTFPL v2](http://www.wtfpl.net/txt/copying/) unless otherwise specified.
-
-
-
-
-
-
+1. 取消了选课未开放会退出程序的设定，能力更加变态；
+2. 使用小猴的验证码识别训练集代替原来的tesseract，不再需要各种依赖包，装一个Pillow即可；
+3. 改成单文件版本，更方便使用；
--- main.py
+++ main.py
@@ -1,23 +1,5 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-#!/usr/bin/python  
-#import urllib.request
-
-########################################################################
-#           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                   Version 2, December 2004
-#
-#       Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
-#
-#   Everyone is permitted to copy and distribute verbatim or modified
-#   copies of this license document, and changing it is allowed as long
-#   as the name is changed.
-#
-#           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-#
-#            0. You just DO WHAT THE FUCK YOU WANT TO.
-########################################################################
-
 
 import HTMLParser  
 import urlparse  
@@ -25,84 +7,153 @@ import urllib
 import urllib2  
 import cookielib  
 import string  
+import io
 import re
 import time
 import sys
+import PIL
+from PIL import Image
+
 reload(sys)
-sys.setdefaultencoding( "utf-8" )
+sys.setdefaultencoding("utf-8")
 
-def loginIn(userName, passWord, inputCaptcha = True):
+
+STANDARD = [
+    [18.714286, 27.142857, 32.857143, 36.714286, 38.285714, 41.571429, 43.000000, 44.714286, 46.285714, 48.285714,
+     48.285714, 36.428571, 27.714286, 24.714286, 23.285714, 22.285714, 21.428571, 19.428571, 19.285714, 19.000000,
+     18.857143, 18.714286, 19.714286, 20.571429, 20.714286, 22.714286, 26.571429, 28.000000, 35.571429, 46.714286,
+     46.714286, 44.857143, 43.000000, 41.285714, 39.571429, 36.142857, 34.428571, 31.000000, 26.000000, 18.714286],
+    [0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000,
+     0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000,
+     0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000,
+     0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],
+    [8.333333, 8.333333, 16.166667, 23.166667, 24.166667, 26.000000, 27.000000, 27.833333, 27.500000, 29.000000,
+     29.500000, 30.333333, 30.000000, 30.000000, 28.666667, 27.666667, 27.333333, 27.833333, 28.000000, 28.333333,
+     29.833333, 31.333333, 33.333333, 35.166667, 38.000000, 43.000000, 41.833333, 40.833333, 39.666667, 37.666667,
+     36.833333, 34.500000, 32.333333, 29.500000, 26.500000, 21.500000, 7.333333, 7.166667, 7.166667, 7.333333],
+    [8.200000, 8.400000, 8.400000, 8.200000, 13.200000, 18.800000, 19.000000, 19.400000, 19.000000, 26.000000,
+     26.200000, 26.400000, 25.400000, 26.200000, 26.000000, 26.400000, 27.400000, 27.200000, 28.400000, 29.200000,
+     31.200000, 33.200000, 36.400000, 41.400000, 46.800000, 48.800000, 52.600000, 51.200000, 49.200000, 48.200000,
+     45.200000, 40.600000, 37.600000, 33.600000, 25.800000, 18.000000, 13.800000, 7.000000, 7.000000, 7.200000],
+    [17.250000, 19.000000, 20.500000, 21.875000, 24.125000, 25.375000, 26.375000, 28.250000, 28.500000, 28.125000,
+     28.000000, 27.750000, 25.875000, 25.250000, 25.125000, 26.250000, 25.000000, 24.625000, 25.625000, 24.750000,
+     24.500000, 24.500000, 25.375000, 24.500000, 52.000000, 52.250000, 52.750000, 52.875000, 53.750000, 55.375000,
+     55.125000, 55.000000, 54.625000, 18.625000, 18.375000, 18.375000, 18.250000, 17.750000, 17.375000, 9.875000],
+    [8.428571, 8.285714, 8.142857, 8.285714, 8.428571, 8.285714, 14.000000, 36.285714, 36.142857, 35.857143, 36.000000,
+     36.000000, 36.142857, 31.714286, 27.000000, 26.857143, 27.714286, 27.571429, 27.714286, 28.714286, 27.857143,
+     29.142857, 29.714286, 31.000000, 34.142857, 41.000000, 40.142857, 39.285714, 38.000000, 37.857143, 36.428571,
+     35.000000, 33.571429, 32.000000, 28.428571, 15.714286, 8.285714, 8.285714, 8.285714, 8.142857],
+    [8.142857, 20.571429, 27.857143, 32.285714, 35.857143, 38.428571, 40.857143, 44.000000, 46.000000, 46.857143,
+     47.571429, 49.142857, 37.000000, 32.857143, 29.857143, 27.428571, 25.714286, 26.285714, 25.571429, 24.714286,
+     25.428571, 24.285714, 24.714286, 24.285714, 26.142857, 27.428571, 28.571429, 31.142857, 34.285714, 41.142857,
+     40.857143, 39.142857, 37.571429, 36.571429, 34.571429, 32.857143, 24.000000, 21.714286, 16.428571, 8.000000],
+    [8.428571, 8.285714, 8.142857, 8.285714, 8.285714, 8.285714, 16.285714, 16.285714, 19.000000, 21.857143, 24.000000,
+     25.285714, 25.857143, 27.714286, 29.857143, 31.000000, 32.857143, 34.571429, 35.285714, 32.714286, 30.142857,
+     29.571429, 29.142857, 28.142857, 27.285714, 27.000000, 26.000000, 26.857143, 26.428571, 26.285714, 26.428571,
+     26.714286, 25.142857, 24.571429, 23.571429, 21.571429, 20.857143, 18.714286, 17.571429, 17.000000],
+    [7.500000, 7.500000, 13.833333, 18.833333, 21.333333, 29.666667, 36.500000, 40.333333, 43.333333, 47.166667,
+     49.833333, 52.833333, 48.666667, 45.833333, 37.500000, 33.500000, 30.833333, 30.000000, 28.000000, 27.500000,
+     28.666667, 27.666667, 28.500000, 29.166667, 32.833333, 33.833333, 36.000000, 40.333333, 47.666667, 52.166667,
+     49.166667, 47.166667, 44.166667, 40.666667, 37.166667, 32.500000, 27.000000, 18.833333, 14.833333, 8.000000],
+    [8.142857, 15.857143, 20.285714, 24.142857, 30.428571, 32.571429, 34.142857, 36.285714, 38.142857, 38.000000,
+     39.571429, 32.428571, 29.571429, 27.571429, 26.571429, 25.714286, 25.714286, 23.714286, 23.857143, 23.428571,
+     24.428571, 23.142857, 23.000000, 26.285714, 26.285714, 26.428571, 30.000000, 33.857143, 39.000000, 50.142857,
+     49.285714, 47.857143, 47.428571, 45.571429, 44.285714, 41.142857, 38.714286, 34.142857, 29.428571, 20.714286]
+]
+
+
+def getCheckCode():
+    for i in range(10):
+        try:
+            image = urllib2.urlopen('http://xk.urp.seu.edu.cn/jw_css/getCheckCode', timeout = 10)
+            break
+        except Exception, e:
+            print e
+            continue
+    else:
+        print u'网络连接失败，请重试'
+        return
+
+    img = Image.open(io.BytesIO(image.read()))
+    start = [13, 59, 105, 151]
+    result = ''
+    for i in start:
+        sample = []
+        for i in xrange(i, i + 40):
+            temp = 0
+            for j in xrange(0, 100):
+                temp += (img.getpixel((i, j))[1] < 40)
+            sample.append(temp)
+        min_score = 1000
+        max_match = 0
+        for idx, val in enumerate(STANDARD):
+            diff = []
+            for i in xrange(len(sample)):
+                diff.append(sample[i] - val[i])
+            avg = float(sum(diff)) / len(diff)
+
+            for i in xrange(len(sample)):
+                diff[i] = abs(diff[i] - avg)
+            score = sum(diff)
+            if score < min_score:
+                min_score = score
+                max_match = idx
+
+        result = result + str(max_match)
+    return (result, img)
+
+
+def loginIn(userName, passWord):
     #设置cookie处理器
-	cj = cookielib.LWPCookieJar()
-	cookie_support = urllib2.HTTPCookieProcessor(cj)
-	opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)  
-	urllib2.install_opener(opener)  
-    #打开选课页面
-#	h = urllib2.urlopen('http://xk.urp.seu.edu.cn/jw_css/system/showLogin.action', timeout = 10)   # seems it is not necessary
-    #获取验证码
+    cj = cookielib.LWPCookieJar()
+    cookie_support = urllib2.HTTPCookieProcessor(cj)
+    opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)  
+    urllib2.install_opener(opener)
     
-	for i in range(10):
-		try:
-			image = urllib2.urlopen('http://xk.urp.seu.edu.cn/jw_css/getCheckCode', timeout = 10)
-			break
-		except Exception, e:
-			print e
-			continue
-	else:
-		return (False, "验证码获取失败", '')
-
-	f = open('code.jpg','wb')
-	f.write(image.read())
-	f.close()
-
-	if inputCaptcha == True:  # manually input the capthcha
-	#读取验证码
-		code = raw_input(u'请打开我所在目录下的code.jpg，并在这里敲入里面的四位数字验证码：')
-		# code = raw_input(u'请打开我所在目录下的code.jpg，并在这里敲入里面的四位数字验证码：'.encode('gbk'))  # used for exporting to exe
-
+    (code, img) = getCheckCode()
+    while len(code) != 4:
+        (code, img) = getCheckCode()
+    print u"验证码识别为： " + code
 
     #构造post数据
-	posturl = 'http://xk.urp.seu.edu.cn/jw_css/system/login.action' 
-	header ={   
-		'Host' : 'xk.urp.seu.edu.cn',   
-		'Proxy-Connection' : 'keep-alive',
-		'Origin' : 'http://xk.urp.seu.edu.cn',
-		'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20100101 Firefox/14.0.1',
-		'Referer' : 'http://xk.urp.seu.edu.cn/jw_css/system/login.action'
-		}
-	data = {
-		'userId' : userName,
-		'userPassword' : passWord, #你的密码，  
-		'checkCode' : code,           #验证码 
-		'x' : '33',     #别管
-		'y' : '5'       #别管2
-		}
+    posturl = 'http://xk.urp.seu.edu.cn/jw_css/system/login.action' 
+    header ={   
+        'Host' : 'xk.urp.seu.edu.cn',   
+        'Proxy-Connection' : 'keep-alive',
+        'Origin' : 'http://xk.urp.seu.edu.cn',
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20100101 Firefox/14.0.1',
+        'Referer' : 'http://xk.urp.seu.edu.cn/jw_css/system/login.action'
+        }
+    data = {
+        'userId' : userName,
+        'userPassword' : passWord, #你的密码，  
+        'checkCode' : code,           #验证码 
+        'x' : '33',     #别管
+        'y' : '5'       #别管2
+        }
     
     #post登录数据
-	(state, text) = postData(posturl,header,data)
-	url = ''
-	if state == True:
-		if (text.find('选课批次') != -1):  # a bad label; the url returned should be the best
-			print u"登录成功"
-			function = re.search(r'onclick="changeXnXq.*\)"', text); # find the function whose parameter are wanted
-			function = function.group()		
-			parameters = re.search(r"'(.*)','(.*)','(.*)'\)", function) # url parameters
-			url = "http://xk.urp.seu.edu.cn/jw_css/xk/runXnXqmainSelectClassAction.action?Wv3opdZQ89ghgdSSg9FsgG49koguSd2fRVsfweSUj=Q89ghgdSSg9FsgG49koguSd2fRVs&selectXn=" + parameters.group(1) + "&selectXq=" + parameters.group(2) + "&selectTime=" + parameters.group(3)
-		else:
-			state = False
-			errorMessage = re.search(r'id="errorReason".*?value="(.*?)"', text)
-			text = errorMessage.group(1)
-	else:
-		text = "网络错误，登录失败" 
-	return (state, text, url)
+    (state, text) = postData(posturl,header,data)
+    url = ''
+    if state == True:
+        if (text.find('选课批次') != -1):  # a bad label; the url returned should be the best
+            print u"登录成功"
+            function = re.search(r'onclick="changeXnXq.*\)"', text); # find the function whose parameter are desired
+            function = function.group()     
+            parameters = re.search(r"'(.*)','(.*)','(.*)'\)", function) # fetch url parameters
+            url = "http://xk.urp.seu.edu.cn/jw_css/xk/runXnXqmainSelectClassAction.action?Wv3opdZQ89ghgdSSg9FsgG49koguSd2fRVsfweSUj=Q89ghgdSSg9FsgG49koguSd2fRVs&selectXn=" + parameters.group(1) + "&selectXq=" + parameters.group(2) + "&selectTime=" + parameters.group(3)
+        else:
+            state = False
+            errorMessage = re.search(r'id="errorReason".*?value="(.*?)"', text)
+            text = errorMessage.group(1)
+    else:
+        text = "网络错误，登录失败" 
+    return (state, text, url)
 
 def selectSemester(semesterNum, url):
     print u"切换学期菜单中......"
     time.sleep(5)
-    #构造选择学期的包
-    # !!!NOTICE: SELECTTIME manually set this url is not a wise choice
-    # geturl ='http://xk.urp.seu.edu.cn/jw_css/xk/runXnXqmainSelectClassAction.action?Wv3opdZQ89ghgdSSg9FsgG49koguSd2fRVsfweSUj=Q89ghgdSSg9FsgG49koguSd2fRVs&selectXn=2014&selectXq='+str(semesterNum)+'&selectTime=2014-05-30%2013:30~2014-06-07%2023:59'
-    
+
     geturl = re.sub('selectXq=.', 'selectXq='+str(semesterNum), url)
     
     header = {  'Host' : 'xk.urp.seu.edu.cn',
@@ -114,7 +165,7 @@ def selectSemester(semesterNum, url):
     #get获取学期课程
     (state, text) = getData(geturl,header,data)
     if state == True:
-        if text.find("数据异常") != -1:  # switch to an unavailable semester
+        if text.find("数据异常") != -1:  # switched to an unavailable semester
             state = False
             text = "目前无法选择学期" + str(semesterNum)
     return (state, text)
@@ -199,6 +250,7 @@ def Mode1(semesterNum, url):
         if total == 0:
             print u"目前没有课可以选择"
             break
+        time.sleep(3)#sleep
         times = times +1
         print u"\n第"+str(times)+u"次选课，已经成功选择"+str(success)+u"门"
         for course in courseList:
@@ -318,7 +370,7 @@ def checkRwState(text):
     if text.find('冲突') != -1:
         return 2
     return -1
-def Mode3(semesterNum, url):
+def Mode3(semesterNum, url):    
     (state, text) = selectSemester(semesterNum, url)
     if state == False:
         print text.decode('utf-8')
@@ -370,7 +422,6 @@ def Mode3(semesterNum, url):
         #print courseYmList
         #找出可以选的课程编号
         courseAva = [i for i in courseTemp if (i not in courseYmList) ]
-        print courseAva
         #选课了
         if len(courseAva) == 0:
             print u"第"+str(times)+u"次刷新，每门课都选不了.."
@@ -402,47 +453,35 @@ def Mode3(semesterNum, url):
 if __name__ == "__main__":
     print u"\n\n\n\n"
     print u"===================================================================== "
-    print u"                    Seu_Jwc_Fker 东南大学选课助手\n"
-    print u"     访问 github.com/SnoozeZ/seu_jwc_fker 以了解本工具的最新动态"
+    print u"                 Seu_Jwc_Fker 东南大学选课助手变态版"
     print u"===================================================================== "
     print u"请选择模式："
     print u"1. 同院竞争臭表脸模式：只值守主界面本院的所有“服从推荐”课程"
     print u"2. 孤注一掷模式：只值守子界面“人文社科类”中你指定一门课程"
     print u"3. 暴力模式：值守子界面“人文社科类”任意一门课程，有剩余就选上"
-    #print u"4. 只值守子界面“自然科学与技术科学类”中的指定一门课程（开发中）"
-    #print u"5. 输入指定任意门课程的名字并值守（课程类型不限）（开发中）"
     
     mode = input(u'\n请输入模式编号(如:1)：')
     userId = raw_input(u'请输入一卡通号(如:213111111)：')
     passWord = raw_input(u'请输入密码(如:65535)：')
     semester = input(u'请输入学期编号(短学期为1，秋季学期为2，春季学期为3)：')
 
-    # used for exporting to exe. damn you cmd
-    # mode = input(u'\n请输入模式编号(如:1)：'.encode('gbk'))
-    # userId = raw_input(u'请输入一卡通号(如:213111111)：'.encode('gbk'))
-    # passWord = raw_input(u'请输入密码(如:65535)：'.encode('gbk'))
-    # semester = input(u'请输入学期编号(短学期为1，秋季学期为2，春季学期为3)：'.encode('gbk'))
-
-    inputCaptcha = True
-    (state, text, url) = loginIn(userId,passWord, inputCaptcha)
-    failTimes = 0
-    if inputCaptcha == True and state == False:
+    (state, text, url) = loginIn(userId,passWord)
+    while state == False or text.find('尚未开放') != -1 or text.find('验证码错误') == -1:
         print text.decode('utf-8')
+        (state, text, url) = loginIn(userId, passWord)
 
     if state == True:
         if 1 == mode:
             Mode1(semester, url)
         if 2 == mode:
             courseName = raw_input(u'请输入你想值守的人文课名称或者其关键词（如:音乐鉴赏）：')
-            # courseName = raw_input(u'请输入你想值守的人文课名称或者其关键词（如:音乐鉴赏）：'.encode('gbk'))  # used for exporting to exe
             try:
                 courseName.decode('utf-8')
             except:
-                courseName.decode('gbk').encode('utf-8')  #handle the input from cmd
+                courseName.decode('gbk').encode('utf-8')
             Mode2(semester,courseName, url)
         if 3 == mode:
             Mode3(semester, url)
     else:
-        print u"要不试试退出后重新打开一下本程序？"
-    input(u'按任意键退出')    
-    # input(u'按任意键退出'.encode('gbk'))  #used for exporting to exe
+        print u"要不试试退出后重启一下本程序？"
+    raw_input(u'按任意键退出')
